189ddb6b6a25b7d45ea832b29a40112bfb2a02cc
Reydner96 {Reyd} <31974666+Reydner96@users.noreply.github.com>
2023-03-24 00:28:33 -0300

    estudando

---
+++ 2.3_Comentarios/comentarios.java
@@ -0,0 +1,10 @@
+public class comentarios {
+	
+	public static void main(String[] args) {
+		// a linha a baixo envia uma msg
+		System.out.println("Comentários no Java_Poo");
+	}
+}
+
+/* Outra forma de 
+escrever um comentário */

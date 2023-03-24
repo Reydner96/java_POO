8bce78b03a03ed50d05268d47a81af3e2109d0e8
Reydner96 {Reyd} <31974666+Reydner96@users.noreply.github.com>
2023-03-24 16:37:57 -0300

    estudando

---
+++ 2.11_tiposPrimitivos/tiposPrimitivos.java
@@ -0,0 +1,23 @@
+public class tiposPrimitivos {
+
+  public static void main(String[] args) {
+    double precoProduto = 9.59;
+
+    boolean alunoMatriculado = true;
+    boolean clienteBloqueado = false;
+
+    char turmaAluno1 = 'A';
+    char tipoCliente = '2';
+    char simbolo = '@';
+
+    long populacaoUberlandia = 700000;
+      System.out.println(populacaoUberlandia);
+
+    long populacaoMundial = 7000000000L;
+      System.out.println(populacaoMundial);
+
+    float saldoConta = 3492.40F;
+      System.out.println(saldoConta);
+  }
+
+}

e68e2b43ed6de22da5612306a633f121498b1dc1
Reydner96 {Reyd} <31974666+Reydner96@users.noreply.github.com>
2023-06-28 13:08:33 -0300

    curso em andamento

---
+++ exerciciosJava/lataOleo/LataOleo.java
@@ -0,0 +1,11 @@
+public class LataOleo {
+    public static void main(String[] args) {
+        double V, R, A;
+            R = 4;
+            A = 8;
+
+            V = 3.1415 * R * R * A;
+
+            System.out.println("O volume da Lata de óleo é de: " + V + "m³");
+    }
+}
